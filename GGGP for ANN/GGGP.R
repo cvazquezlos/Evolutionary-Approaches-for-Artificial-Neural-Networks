8d05fa4f436e4c8af0ab7bfa82c6af7a8e2b6323
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-12-07 22:16:37 +0100

    Added basic ANN structure.

--- GGGP for ANN/GGGP.R
+++ GGGP for ANN/GGGP.R
@@ -1,4 +1,6 @@
 library("gramEvol")
+library("neuralnet")
+
 INPUT = 2
 OUTPUT = 3
 
@@ -10,6 +12,35 @@ grammar <- list(
   n = gsrule("n<n>", "n")
 )
 
+# https://datascienceplus.com/neuralnet-train-and-test-neural-networks-using-r/
+# http://www.parallelr.com/r-deep-neural-network-from-scratch/
 grammarDef <- CreateGrammar(grammar)
 print(grammarDef)
 
+# evaluation <- function(l) {
+#   layers <- strsplit(l, "/")[[1]]
+#   i <- 0
+#   hidden <- list() # Contains the number of hidden layers and the number of neurons of each hidden layer.
+#   for(j in head(layers, -1)) {
+#     if (i!=0) {hidden[i] <- nchar(j)}
+#     i <- i+1
+#   }
+# }
+
+evaluation <- function(l) {
+  layers <- strsplit(l, "/")[[1]]
+  i <- 0
+  hidden_l <- numeric(0) # Contains the number of hidden layers and the number of neurons of each hidden layer.
+  for(j in head(layers, -1)) {
+    if (i!=0) {hidden_l[i] <- nchar(j)}
+    i <- i+1
+  }
+  
+  # https://medium.com/analytics-vidhya/build-your-first-neural-network-model-on-a-structured-dataset-using-keras-d9e7de5c6724
+  input <- NULL
+  output <- NULL
+  trainingdata <- NULL
+  nn <- neuralnet(output~input, trainingdata, hidden=hidden_l, threshold=0.01)
+  nn$result.matrix
+  plot(nn)
+}
