7fe4c4fc5bd4e9c91da3d3870dfae8ba0327336d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-01-04 23:08:51 +0100

    Added some variables.

--- GGGP/genetic-program.R
+++ GGGP/genetic-program.R
@@ -24,14 +24,24 @@ y_test <- NULL
 classification_type <- 1 # -1: Regression
                          # 0: Single-label classification
                          # 1: Multi-label classification
+
+# Data to save
+# 1. Accuracy of each final solution: train, validation and test.
 sol_train_accuracy <- list()
-avg_train_accuracy <- list()
 sol_validation_accuracy <- list()
-avg_validation_accuracy <- list()
 sol_test_accuracy <- list()
+# 2. Average accuracy of the whole individuals of the GP.
+avg_train_accuracy <- list()
+avg_validation_accuracy <- list()
 avg_test_accuracy <- list()
-sol_nn_structure <- list()
+# 4. Solution architecture.
+sol_nn_architecture <- list()
+# 5. Name of the saved model at ./models.
 sol_model_name <- list()
+# 6. Both loss and accuracy data at each epoch of the solution.
+sol_plot_data <- list()
+# 7. Execution time.
+exec_time <- list()
 
 data_cleaning <- function(url, sep) {
   data <<- read.csv(url, header=T, sep=sep)
@@ -101,13 +111,14 @@ evaluation <- function(word) {
   }
   # https://keras.rstudio.com/articles/training_callbacks.html - STOP TRAINING.
   history <- model %>% fit(X_train, y_train, epochs = epochs, verbose = 0)
-  history_df <<- as.data.frame(history)
+  #toJSON(history_df) # Is the JSON that is needed.
   if (mode == 0) {
     score <- model %>% evaluate(X_validation, y_validation)
     return(score['loss'][[1]])
   } else {
     score <- model %>% evaluate(X_test, y_test)
     save_model_hdf5(model, paste0('iris_model_', j, '.h5'))
+    sol_plot_data <- append(sol_plot_data, toJSON(history_df))
     return(score['loss'][[1]])
   }
 }
@@ -128,8 +139,12 @@ grammarDef <- CreateGrammar(grammar)
 
 data_cleaning("../datasets/classification/iris.csv", ",")
 for (i in c(1:100)) {
+  start_time <- Sys.time()
   optimal_word <- GrammaticalEvolution(grammarDef, evaluation, 1, mutationChance=0.05, monitorFunc = monitor)
-  
+  end_time <- Sys.time()
+  hidden_layers_optimal_word <- extract_neurons(optimal_word)
+  sol_nn_architecture <- append(sol_nn_architecture, hidden_layers_optimal_word)
+  exec_time <- append(exec_time, end_time - start_time)
 }
 optimal_word <- GrammaticalEvolution(grammarDef, evaluation, 1, mutationChance=0.05, monitorFunc = monitor)
 hidden_layers_optimal_word <- extract_neurons(optimal_word)
