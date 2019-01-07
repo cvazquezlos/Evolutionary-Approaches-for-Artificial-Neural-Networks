ec5e70ad405dc22ef087e2f61fec60ed16f551b8
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-01-07 20:19:04 +0100

    Updated algorithm.

--- GGGP/genetic-program.R
+++ GGGP/genetic-program.R
@@ -5,15 +5,18 @@ library("keras")
 library("neuralnet")
 library("stringr")
 
+classification_type <- 1 # -1: Regression, 0: Single-label classification, 1: Multi-label classification
 epochs <- 100
 data <- NULL
+gen_no <- 1
+gen_pop_err <- list()
+gen_evolution <- NULL
 I <- NULL
 j <- 1
 input <- NULL
 mode <- 0
 O <- NULL
 output <- NULL
-train <- NULL
 X_train <- NULL      # 70%
 y_train <- NULL
 X_validation <- NULL # 20%
@@ -21,27 +24,21 @@ y_validation <- NULL
 X_test <- NULL       # 10%
 y_test <- NULL
 
-classification_type <- 1 # -1: Regression
-                         # 0: Single-label classification
-                         # 1: Multi-label classification
-
 # Data to save
-# 1. Accuracy of each final solution: train, validation and test.
-sol_train_accuracy <- list()
-sol_validation_accuracy <- list()
-sol_test_accuracy <- list()
-# 2. Average accuracy of the whole individuals of the GP.
-avg_train_accuracy <- list()
-avg_validation_accuracy <- list()
-avg_test_accuracy <- list()
-# 4. Solution architecture.
-sol_nn_architecture <- list()
-# 5. Name of the saved model at ./models.
-sol_model_name <- list()
-# 6. Both loss and accuracy data at each epoch of the solution.
-sol_plot_data <- list()
-# 7. Execution time.
-exec_time <- list()
+# 1. GP population evolution.
+gp_plot_data <- NULL
+# 2. Accuracy of each final solution: train, validation and test.
+sol_train_accuracy <- NULL
+sol_validation_accuracy <- NULL
+sol_test_accuracy <- NULL
+# 3. Solution architecture.
+sol_nn_architecture <- NULL
+# 4. Name of the saved model at ./models.
+sol_model_name <- NULL
+# 5. Both loss and accuracy data at each epoch of the solution.
+sol_plot_data <- NULL
+# 6. Execution time.
+exec_time <- NULL
 
 data_cleaning <- function(url, sep) {
   data <<- read.csv(url, header=T, sep=sep)
@@ -61,7 +58,7 @@ data_cleaning <- function(url, sep) {
     shuffled_df <- as.data.frame(data[sample(n),])
   }
   colnames(shuffled_df) <- gsub("[^a-zA-Z]*", "", colnames(shuffled_df))
-  train <<- shuffled_df[1: round(0.7*n),]
+  train <- shuffled_df[1: round(0.7*n),]
   validation <- shuffled_df[(round(0.7*n)+1):round(0.9*n),]
   test <- shuffled_df[(round(0.9*n)+1):n,]
   X_train <<- train[,head(colnames(shuffled_df), -3)] %>% as.matrix()
@@ -86,7 +83,7 @@ extract_neurons <- function(word) {
   }
   return(hidden_l)
 }
-history_df <- NULL
+
 evaluation <- function(word) {
   hidden_layers <- extract_neurons(word)
   model <- keras_model_sequential()
@@ -111,21 +108,35 @@ evaluation <- function(word) {
   }
   # https://keras.rstudio.com/articles/training_callbacks.html - STOP TRAINING.
   history <- model %>% fit(X_train, y_train, epochs = epochs, verbose = 0)
-  #toJSON(history_df) # Is the JSON that is needed.
   if (mode == 0) {
     score <- model %>% evaluate(X_validation, y_validation)
+    gen_pop_err <<- c(gen_pop_err, score['loss'][[1]])
     return(score['loss'][[1]])
   } else {
+    gp_plot_data <<- toJSON(gen_evolution)
+    history_df <- as.data.frame(history)
+    score <- model %>% evaluate(X_train, y_train)
+    sol_train_accuracy <<- score['acc'][[1]]
+    score <- model %>% evaluate(X_validation, y_validation)
+    sol_validation_accuracy <<- score['acc'][[1]]
     score <- model %>% evaluate(X_test, y_test)
-    save_model_hdf5(model, paste0('iris_model_', j, '.h5'))
-    sol_plot_data <- append(sol_plot_data, toJSON(history_df))
+    sol_test_accuracy <<- score['acc'][[1]]
+    model_name <- paste0('../results/models/iris_model_', j, '.h5')
+    save_model_hdf5(model, model_name)
+    sol_plot_data <<- toJSON(history_df)
+    sol_model_name <<- model_name
     return(score['loss'][[1]])
   }
 }
 
 monitor <- function(results){
+  pop_train <- Reduce("+", gen_pop_err) / length(gen_pop_err)
+  best_train <- evaluation(results$best$expressions[[1]])
+  gen_evolution <<- c(gen_evolution, toJSON(data.frame(gen_no, pop_train, best_train)))
   cat("--------------------\n")
   print(results)
+  gen_pop_err <<- list()
+  gen_no <<- gen_no + 1
 }
 
 grammar <- list(
@@ -135,18 +146,33 @@ grammar <- list(
   h = gsrule("<h><h>", "/<n>"),
   n = gsrule("n<n>", "n")
 )
-grammarDef <- CreateGrammar(grammar)
 
+grammarDef <- CreateGrammar(grammar)
 data_cleaning("../datasets/classification/iris.csv", ",")
-for (i in c(1:100)) {
+results <- data.frame(gp_plot_data = character(),
+                      sol_train_accuracy = double(),
+                      sol_validation_accuracy = double(),
+                      sol_test_accuracy = double(),
+                      sol_nn_architecture = character(),
+                      sol_model_name = character(),
+                      sol_plot_data = character(),
+                      exec_time = double(),
+                      stringsAsFactors = FALSE)
+for (i in c(1:30)) {
+  mode <- 0
+  gen_evolution <- list()
   start_time <- Sys.time()
-  optimal_word <- GrammaticalEvolution(grammarDef, evaluation, 1, mutationChance=0.05, monitorFunc = monitor)
+  optimal_word <- GrammaticalEvolution(grammarDef, evaluation, mutationChance = 0.05, monitorFunc = monitor)
   end_time <- Sys.time()
+  mode <- 1
   hidden_layers_optimal_word <- extract_neurons(optimal_word)
-  sol_nn_architecture <- append(sol_nn_architecture, hidden_layers_optimal_word)
-  exec_time <- append(exec_time, end_time - start_time)
+  sol_nn_architecture <- paste(I, paste0(hidden_layers_optimal_word, collapse = ":"), O, sep = ":")
+  evaluation(optimal_word)
+  exec_time <- as.double(toString(end_time - start_time))
+  gen_no <- 1
+  j <- j + 1
+  results[nrow(results) + 1,] <- c(gp_plot_data, sol_train_accuracy, sol_validation_accuracy, sol_test_accuracy, sol_nn_architecture, sol_model_name,
+                                   sol_plot_data, exec_time)
 }
-optimal_word <- GrammaticalEvolution(grammarDef, evaluation, 1, mutationChance=0.05, monitorFunc = monitor)
-hidden_layers_optimal_word <- extract_neurons(optimal_word)
-mode <- 1
-print(evaluation(optimal_word))
+write.csv(results, file = "../results/iris.csv") # Empty CSV.
+#write.table(results, "../results/iris.csv", sep = ",", col.names = F, append = T)
